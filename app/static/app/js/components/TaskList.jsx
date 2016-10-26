710b36e325033b06bbed95795239401e7ba79eed
Piero Toffanin <pt@masseranolabs.com>
2016-10-26 17:35:31 -0400

    Started working on task list

--- app/static/app/js/components/TaskList.jsx
+++ app/static/app/js/components/TaskList.jsx
@@ -4,20 +4,51 @@ import '../css/TaskList.scss';
 class TaskList extends React.Component {
   constructor(){
     super();
+
+    this.state = {
+      tasks: []
+    };
   }
 
   componentDidMount(){
-    console.log("DISPLAY");
+    this.loadTaskList(); 
   }
 
   refresh(){
-    console.log("REFRESH");
+    this.loadTaskList();
+  }
+
+  loadTaskList(){
+    this.taskListRequest = 
+      $.getJSON(this.props.source, json => {
+                if (json.results){
+                    this.setState({
+                        projects: json.results,
+                        loading: false
+                    });
+                }else{
+                    this.setState({ 
+                        error: `Invalid JSON response: ${JSON.stringify(json)}`,
+                        loading: false
+                    });
+                }
+            })
+            .fail((jqXHR, textStatus, errorThrown) => {
+                this.setState({ 
+                    error: `Could not load projects list: ${textStatus}`,
+                    loading: false
+                });
+            });
+  }
+
+  componentWillUnmount(){
+    this.taskListRequest.abort();
   }
 
   render() {
     return (
       <div className="task-list">
-        <span>Updating task list... <i className="fa fa-refresh fa-spin fa-fw"></i></span>
+        <span>Loading... <i className="fa fa-refresh fa-spin fa-fw"></i></span>
       </div>
     );
   }
