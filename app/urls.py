2d1bda4017ff06d8283c5aed03aff45d850f1bd8
Piero Toffanin <pt@masseranolabs.com>
2016-11-16 15:37:35 -0500

    Added swagger viewer

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url, include
+from rest_framework_swagger.views import get_swagger_view
+
 from . import views
 from app.boot import boot
 from webodm import settings
@@ -12,6 +14,7 @@ urlpatterns = [
     url(r'^processingnode/([\d]+)/$', views.processing_node, name='processing_node'),
 
     url(r'^api/', include("app.api.urls")),
+    url(r'^apiviewer/', get_swagger_view(title='WebODM API'))
 ]
 
 # Test cases call boot() independently
