bb2d87130b88fae55cc1d5b71d32c19eec7ef483
Piero Toffanin <pt@masseranolabs.com>
2017-07-20 10:19:25 -0400

    Added Preset model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .image_upload import ImageUpload, image_directory_path
 from .project import Project
 from .task import Task, validate_task_options, gcp_directory_path
+from .preset import Preset
