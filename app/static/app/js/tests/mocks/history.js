cf641d9995a9380a06943625aa37d25b3433ccca
Piero Toffanin <pt@masseranolabs.com>
2017-09-29 15:05:52 -0400

    Removed history mock file (unused), lowercase comment

--- app/static/app/js/tests/mocks/history.js
+++
@@ -1,3 +0,0 @@
-// TODO: how?
-
-export default {};
--- app/static/app/js/tests/setup/browserMock.js
+++ app/static/app/js/tests/setup/browserMock.js
@@ -1,5 +1,5 @@
 // Define certain DOM elements
-// that are not defined in the JEST environment
+// that are not defined in the jest environment
 
 const currentScript = document.createElement('script');
 currentScript.src = "http://bogus";
