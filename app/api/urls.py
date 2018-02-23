f788913d3568c75c67a70d41249e8e39776e0df3
Abdelkoddouss IZem <31613538+Abizem@users.noreply.github.com>
2018-02-23 21:17:39 +0000

    Update urls.py

--- app/api/urls.py
+++ app/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 
 from app.api.presets import PresetViewSet
 from .projects import ProjectViewSet
-from .tasks import TaskViewSet, TaskTiles, TaskTilesJson, TaskDownloads, TaskAssets
+from .tasks import TaskViewSet, TaskTiles, TaskTilesJson, TaskDownloads, TaskAssets, TaskVolume
 from .processingnodes import ProcessingNodeViewSet, ProcessingNodeOptionsView
 from rest_framework_nested import routers
 from rest_framework_jwt.views import obtain_jwt_token
