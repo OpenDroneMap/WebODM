d5b284539dbba222d26dd5735805470d146f31cd
Piero Toffanin <pt@masseranolabs.com>
2018-12-28 15:14:15 -0500

    Lightning network plugin skeleton

---
+++ plugins/lightning/plugin.py
@@ -0,0 +1,18 @@
+from app.plugins import PluginBase, Menu, MountPoint
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+class Plugin(PluginBase):
+    def main_menu(self):
+        return [Menu("Lightning Network", self.public_url(""), "fa fa-bolt fa-fw")]
+
+    def app_mount_points(self):
+        @login_required
+        def diagnostic(request):
+            return render(request, self.template_path("index.html"))
+
+        return [
+            MountPoint('$', diagnostic)
+        ]
+
+
