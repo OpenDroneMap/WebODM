8332624906b48d7bdb27424ab59639641bfbc8fe
Piero Toffanin <pt@masseranolabs.com>
2019-01-15 14:07:35 -0500

    More presets

--- app/boot.py
+++ app/boot.py
@@ -96,6 +96,24 @@ def boot():
 
 def add_default_presets():
     try:
+        Preset.objects.update_or_create(name='3D Model', system=True,
+                                        defaults={'options': [{'name': 'mesh-octree-depth', 'value': "11"},
+                                                              {'name': 'use-3dmesh', 'value': True},
+                                                              {'name': 'depthmap-resolution', 'value': '1000'},
+                                                              {'name': 'mesh-size', 'value': '600000'}]})
+        Preset.objects.update_or_create(name='Buildings', system=True,
+                                        defaults={'options': [{'name': 'mesh-octree-depth', 'value': "10"},
+                                                              {'name': 'mesh-size', 'value': '300000'},
+                                                              {'name': 'depthmap-resolution', 'value': '1000'},
+                                                              {'name': 'texturing-nadir-weight', 'value': "28"}]})
+        Preset.objects.update_or_create(name='Point of Interest', system=True,
+                                        defaults={'options': [{'name': 'matcher-neighbors', 'value': "24"},
+                                                              {'name': 'mesh-size', 'value': '600000'},
+                                                              {'name': 'use-3dmesh', 'value': True}]})
+        Preset.objects.update_or_create(name='Forest', system=True,
+                                        defaults={'options': [{'name': 'min-num-features', 'value': "18000"},
+                                                              {'name': 'matcher-neighbors', 'value': "21"},
+                                                              {'name': 'texturing-data-term', 'value': "area"}]})
         Preset.objects.update_or_create(name='DSM + DTM', system=True,
                                         defaults={
                                             'options': [{'name': 'dsm', 'value': True}, {'name': 'dtm', 'value': True}]})
@@ -103,11 +121,13 @@ def add_default_presets():
                                         defaults={'options': [{'name': 'fast-orthophoto', 'value': True}]})
         Preset.objects.update_or_create(name='High Resolution', system=True,
                                         defaults={'options': [{'name': 'dsm', 'value': True},
-                                                              {'name': 'dem-resolution', 'value': "2.5"},
-                                                              {'name': 'orthophoto-resolution', 'value': "2.5"},
+                                                              {'name': 'depthmap-resolution', 'value': '1000'},
+                                                              {'name': 'dem-resolution', 'value': "2.0"},
+                                                              {'name': 'orthophoto-resolution', 'value': "2.0"},
                                                               ]})
         Preset.objects.update_or_create(name='Default', system=True,
                                         defaults={'options': [{'name': 'dsm', 'value': True}]})
+
     except MultipleObjectsReturned:
         # Mostly to handle a legacy code problem where
         # multiple system presets with the same name were
