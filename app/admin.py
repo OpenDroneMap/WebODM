e11a58739ab518cdf87d1d7df98b0b9ec2613f05
Piero Toffanin <pt@masseranolabs.com>
2019-03-19 15:19:23 -0400

    New Administration menu, plugins panel, changed plugins UI enable/disable workflow

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,14 @@
+from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
 from guardian.admin import GuardedModelAdmin
 
 from app.models import PluginDatum
 from app.models import Preset
+from app.models import Plugin
+from app.plugins import get_plugin_by_name
 from .models import Project, Task, ImageUpload, Setting, Theme
 from django import forms
 from codemirror2.widgets import CodeMirrorEditor
@@ -61,5 +67,71 @@ class ThemeAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Theme, ThemeAdmin)
-
 admin.site.register(PluginDatum, admin.ModelAdmin)
+
+
+class PluginAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "version", "author", "enabled", "plugin_actions")
+    readonly_fields = ("name", )
+
+    def has_add_permission(self, request):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def description(self, obj):
+        manifest = get_plugin_by_name(obj.name, only_active=False).get_manifest()
+        return manifest.get('description', '')
+
+    def version(self, obj):
+        manifest = get_plugin_by_name(obj.name, only_active=False).get_manifest()
+        return manifest.get('version', '')
+
+    def author(self, obj):
+        manifest = get_plugin_by_name(obj.name, only_active=False).get_manifest()
+        return manifest.get('author', '')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        print(urls)
+        custom_urls = [
+            url(
+                r'^(?P<plugin_name>.+)/enable/$',
+                self.admin_site.admin_view(self.plugin_enable),
+                name='plugin-enable',
+            ),
+            url(
+                r'^(?P<plugin_name>.+)/disable/$',
+                self.admin_site.admin_view(self.plugin_disable),
+                name='plugin-disable',
+            ),
+        ]
+        return custom_urls + urls
+
+    def plugin_enable(self, request, plugin_name, *args, **kwargs):
+        p = Plugin.objects.get(pk=plugin_name)
+        p.enabled = True
+        p.save()
+        return HttpResponseRedirect(reverse('admin:app_plugin_changelist'))
+
+    def plugin_disable(self, request, plugin_name, *args, **kwargs):
+        p = Plugin.objects.get(pk=plugin_name)
+        p.enabled = False
+        p.save()
+        return HttpResponseRedirect(reverse('admin:app_plugin_changelist'))
+
+    def plugin_actions(self, obj):
+        return format_html(
+            '<a class="button" href="{}" {}>Disable</a>&nbsp;'
+            '<a class="button" href="{}" {}>Enable</a>',
+            reverse('admin:plugin-disable', args=[obj.pk]) if obj.enabled else '#',
+            'disabled' if not obj.enabled else '',
+            reverse('admin:plugin-enable', args=[obj.pk]) if not obj.enabled else '#',
+            'disabled' if obj.enabled else '',
+        )
+
+    plugin_actions.short_description = 'Actions'
+    plugin_actions.allow_tags = True
+
+
+admin.site.register(Plugin, PluginAdmin)
--- app/plugins/functions.py
+++ app/plugins/functions.py
@@ -11,14 +11,48 @@ from string import Template
 
 from django.http import HttpResponse
 
+from app.models import Plugin
 from app.models import Setting
 from webodm import settings
 
 logger = logging.getLogger('app.logger')
 
-def register_plugins():
-    for plugin in get_active_plugins():
+def init_plugins():
+    build_plugins()
+    sync_plugin_db()
+    register_plugins()
 
+def sync_plugin_db():
+    """
+    Creates db entries for undiscovered plugins to keep track
+    of enabled/disabled plugins
+    """
+    db_plugins = Plugin.objects.all()
+    fs_plugins = get_plugins()
+
+    # Remove plugins that are in the database but not on the file system
+    for db_plugin in db_plugins:
+        fs_found = next((fs_plugin for fs_plugin in fs_plugins if db_plugin.name == fs_plugin.get_name()), None)
+        if not fs_found:
+            Plugin.objects.filter(name=db_plugin.name).delete()
+            logger.info("Cleaned [{}] plugin from database (not found in file system)".format(db_plugin.name))
+
+    # Add plugins found in the file system, but not yet in the database
+    for plugin in get_plugins():
+        # Plugins that have a "disabled" file are disabled
+        disabled_path = plugin.get_path("disabled")
+        disabled =  os.path.isfile(disabled_path)
+        if not disabled:
+            _, created = Plugin.objects.get_or_create(
+                name=plugin.get_name(),
+                defaults={'enabled': not disabled},
+            )
+            if created:
+                logger.info("Added [{}] plugin to database".format(plugin.get_name()))
+
+
+def build_plugins():
+    for plugin in get_plugins():
         # Check for package.json in public directory
         # and run npm install if needed
         if plugin.path_exists("public/package.json") and not plugin.path_exists("public/node_modules"):
@@ -27,8 +61,6 @@ def register_plugins():
 
         # Check if we need to generate a webpack.config.js
         if len(plugin.build_jsx_components()) > 0 and plugin.path_exists('public'):
-            logger.info("Generating webpack.config.js for {}".format(plugin))
-
             build_paths = map(lambda p: os.path.join(plugin.get_path('public'), p), plugin.build_jsx_components())
             paths_ok = not (False in map(lambda p: os.path.exists, build_paths))
 
@@ -48,14 +80,17 @@ def register_plugins():
                     with open(plugin.get_path('public/webpack.config.js'), 'w') as f:
                         f.write(wpc_content)
             else:
-                logger.warning("Cannot generate webpack.config.js for {}, a path is missing: {}".format(plugin, ' '.join(build_paths)))
-
+                logger.warning(
+                    "Cannot generate webpack.config.js for {}, a path is missing: {}".format(plugin, ' '.join(build_paths)))
 
         # Check for webpack.config.js (if we need to build it)
         if plugin.path_exists("public/webpack.config.js") and not plugin.path_exists("public/build"):
             logger.info("Running webpack for {}".format(plugin.get_name()))
             subprocess.call(['webpack-cli'], cwd=plugin.get_path("public"))
 
+
+def register_plugins():
+    for plugin in get_active_plugins():
         plugin.register()
         logger.info("Registered {}".format(plugin))
 
@@ -94,22 +129,23 @@ def get_api_url_patterns():
 
     return url_patterns
 
-
 plugins = None
-def get_active_plugins():
+def get_plugins():
+    """
+    :return: all plugins instances (enabled or not)
+    """
     # Cache plugins search
     global plugins
     if plugins != None: return plugins
 
-    plugins = []
     plugins_path = get_plugins_path()
+    plugins = []
 
     for dir in [d for d in os.listdir(plugins_path) if os.path.isdir(plugins_path)]:
         # Each plugin must have a manifest.json and a plugin.py
         plugin_path = os.path.join(plugins_path, dir)
-        manifest_path = os.path.join(plugin_path, "manifest.json")
         pluginpy_path = os.path.join(plugin_path, "plugin.py")
-        disabled_path = os.path.join(plugin_path, "disabled")
+        manifest_path = os.path.join(plugin_path, "manifest.json")
 
         # Do not load test plugin unless we're in test mode
         if os.path.basename(plugin_path) == 'test' and not settings.TESTING:
@@ -119,37 +155,50 @@ def get_active_plugins():
         if os.path.basename(plugin_path) == '.gitignore':
             continue
 
+        # Check plugin required files
         if not os.path.isfile(manifest_path) or not os.path.isfile(pluginpy_path):
             logger.warning("Found invalid plugin in {}".format(plugin_path))
             continue
 
-        # Plugins that have a "disabled" file are disabled
-        if os.path.isfile(disabled_path):
-            continue
+        # Instantiate the plugin
+        try:
+            module = importlib.import_module("plugins.{}".format(dir))
+            plugin = (getattr(module, "Plugin"))()
 
-        # Read manifest
-        with open(manifest_path) as manifest_file:
-            manifest = json.load(manifest_file)
+            # Check version
+            manifest = plugin.get_manifest()
             if 'webodmMinVersion' in manifest:
                 min_version = manifest['webodmMinVersion']
 
                 if versionToInt(min_version) > versionToInt(settings.VERSION):
-                    logger.warning("In {} webodmMinVersion is set to {} but WebODM version is {}. Plugin will not be loaded. Update WebODM.".format(manifest_path, min_version, settings.VERSION))
+                    logger.warning(
+                        "In {} webodmMinVersion is set to {} but WebODM version is {}. Plugin will not be loaded. Update WebODM.".format(
+                            manifest_path, min_version, settings.VERSION))
                     continue
 
-        # Instantiate the plugin
-        try:
-            module = importlib.import_module("plugins.{}".format(dir))
-            cls = getattr(module, "Plugin")
-            plugins.append(cls())
+            plugins.append(plugin)
         except Exception as e:
             logger.warning("Failed to instantiate plugin {}: {}".format(dir, e))
 
     return plugins
 
 
-def get_plugin_by_name(name):
-    plugins = get_active_plugins()
+def get_active_plugins():
+    plugins = []
+    enabled_plugins = [p.name for p in Plugin.objects.filter(enabled=True).all()]
+    for plugin in get_plugins():
+        if plugin.get_name() in enabled_plugins:
+            plugins.append(plugin)
+
+    return plugins
+
+
+def get_plugin_by_name(name, only_active=True):
+    if only_active:
+        plugins = get_active_plugins()
+    else:
+        plugins = get_plugins()
+
     res = list(filter(lambda p: p.get_name() == name, plugins))
     return res[0] if res else None
 
