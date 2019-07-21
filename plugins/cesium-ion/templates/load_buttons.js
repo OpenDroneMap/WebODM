ee2eb1a4df4bd0c4ed46127bf3c7be641e7a2544
Piero Toffanin <pt@masseranolabs.com>
2019-07-21 14:11:56 -0400

    Load buttons only if a token is set

--- plugins/cesium-ion/templates/load_buttons.js
+++ plugins/cesium-ion/templates/load_buttons.js
@@ -1,11 +1,13 @@
 PluginsAPI.Dashboard.addTaskActionButton(
 	["{{ app_name }}/build/TaskView.js"],
 	function(args, TaskView) {
-		return React.createElement(TaskView, {
+		if ("{{ token }}"){
+		    return React.createElement(TaskView, {
 			task: args.task,
 			token: "{{ token }}",
 			apiURL: "{{ api_url }}",
 			ionURL: "{{ ion_url }}"
-		});
+		    });
+		}
 	}
 );
