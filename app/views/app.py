ecef6777c17832d45fc8b0bec391c523594a5188
emmadal <emmanueldalougou@gmail.com>
2019-08-23 12:39:29 +0000

    change name first project to DEMO PROJECT if the user don't have a project

--- app/views/app.py
+++ app/views/app.py
@@ -30,7 +30,7 @@ def dashboard(request):
 
     # Create first project automatically
     if Project.objects.count() == 0:
-        Project.objects.create(owner=request.user, name=_("First Project"))
+        Project.objects.create(owner=request.user, name=_("DEMO PROJECT"))
 
     return render(request, 'app/dashboard.html', {'title': 'Dashboard',
         'no_processingnodes': no_processingnodes,
