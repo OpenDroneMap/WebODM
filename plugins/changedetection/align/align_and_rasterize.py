28b512dfdddf899566c24f683a420146c286cd5d
NChamo <nicolas@chamo.com.ar>
2020-04-25 13:51:27 -0300

    Remove align

--- plugins/changedetection/align/align_and_rasterize.py
+++
@@ -1,8 +0,0 @@
-from .rasterize import rasterize_cloud
-from .cloud_compare import run_icp
-
-def rasterize(point_cloud_path, dem_type, raster_path):
-    rasterize_cloud(point_cloud_path, dem_type, raster_path)
-
-def align(reference_pcl_path, pcl_to_modify_path, output_path):
-    run_icp(reference_pcl_path, pcl_to_modify_path, output_path)
