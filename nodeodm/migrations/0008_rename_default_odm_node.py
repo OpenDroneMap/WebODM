42292ba784223eac216bf70efe2d6e99dd574181
Antony Harfield <antonyharfield@gmail.com>
2020-09-09 16:53:35 +0700

    Set default node hostname

--- nodeodm/migrations/0008_rename_default_odm_node.py
+++ nodeodm/migrations/0008_rename_default_odm_node.py
@@ -4,12 +4,14 @@ from __future__ import unicode_literals
 from django.db import migrations
 from nodeodm.models import ProcessingNode
 
+
 def rename_default_node(apps, schema_editor):
     for default_node in ProcessingNode.objects.filter(hostname='node-odm-1'):
-        default_node.hostname = 'webodm_node-odm_1'
+        default_node.hostname = 'dronebox_node-odm_1'
         default_node.label = 'node-odm-1'
         default_node.save()
-    
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
