733591d4990346004a63474084222dade3364dcd
Antony Harfield <antonyharfield@gmail.com>
2020-09-10 11:54:05 +0700

    Add additional fields to registration and fix validation

--- app/views/register.py
+++ app/views/register.py
@@ -1,24 +1,41 @@
 from django import forms
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=30,
+                                 required=False,
+                                 help_text='Optional')
+    last_name = forms.CharField(max_length=30,
+                                required=False,
+                                help_text='Optional')
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = [
+            "username", "first_name", "last_name", "email", "password1",
+            "password2"
+        ]
 
 
-def register(response):
-    if response.method == "POST":
-        form = RegisterForm(response.POST)
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect("/")
+            new_user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+            )
+            login(request, new_user)
+            return redirect("/dashboard")
     else:
+        if request.user.is_authenticated:
+            return redirect("/dashboard")
         form = RegisterForm()
 
-    return render(response, "app/registration/register.html", {"form": form})
+    return render(request, "app/registration/register.html", {"form": form})
