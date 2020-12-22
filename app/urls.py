b5bdb0d840fdf2d3eb39c6635de036ba09bb2130
Piero Toffanin <pt@masseranolabs.com>
2020-12-22 14:07:44 -0500

    More strings, developer mode translation reload

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.views.i18n import JavaScriptCatalog
 
-from .views import app as app_views, public as public_views
+from .views import app as app_views, public as public_views, dev as dev_views
 from .plugins.views import app_view_handler
 
 from app.boot import boot
@@ -40,7 +40,7 @@ urlpatterns = [
     url(r'^plugins/(?P<plugin_name>[^/.]+)/(.*)$', app_view_handler),
 
     url(r'^about/$', app_views.about, name='about'),
-    url(r'^dev-tools/$', app_views.dev_tools, name='dev_tools'),
+    url(r'^dev-tools/(?P<action>.*)$', dev_views.dev_tools, name='dev_tools'),
 
     # TODO: add caching: https://docs.djangoproject.com/en/3.1/topics/i18n/translation/#note-on-performance
     url(r'^jsi18n/', JavaScriptCatalog.as_view(packages=['app']), name='javascript-catalog'),
