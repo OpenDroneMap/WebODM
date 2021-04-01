2417e116d9df383bac200e479f40e425cfb01fbb
= <pedrovm@virt0174>
2021-04-01 20:53:53 -0300

    Add proxy on start script

--- start.sh
+++ start.sh
@@ -41,7 +41,9 @@ if [ "$1" = "--setup-devenv" ] || [ "$2" = "--setup-devenv" ]; then
     
     git submodule update --init
     
-    echo Setup npm dependencies...
+    echo Setup npm dependencies..
+    echo Setting npm proxy environment
+    npm config set proxy http://proxy.cnptia.embrapa.br:3128/
     npm install
     cd nodeodm/external/NodeODM
     npm install
