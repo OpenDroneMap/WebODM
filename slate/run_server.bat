62fb4a77f4e42f8f19b3e433a132690e30920479
Piero Toffanin <pt@masseranolabs.com>
2021-06-07 12:06:05 -0400

    Fix quotes

--- slate/run_server.bat
+++ slate/run_server.bat
@@ -2,4 +2,4 @@ REM Building docker image
 if "%1"=="--build" docker build -t opendronemap/webodm_slate .
 
 REM Launching server
-docker run -ti --rm -v %CD%:/webodm/slate -p 4567:4567 opendronemap/webodm_slate --watcher-force-polling
+docker run -ti --rm -v "%CD%":/webodm/slate -p 4567:4567 opendronemap/webodm_slate --watcher-force-polling
