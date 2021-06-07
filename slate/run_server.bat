67a55604dfee05ffa03953d1fd00b36845a22896
Piero Toffanin <pt@masseranolabs.com>
2021-06-07 12:16:50 -0400

    Edit run_server.bat

--- slate/run_server.bat
+++ slate/run_server.bat
@@ -2,4 +2,4 @@ REM Building docker image
 if "%1"=="--build" docker build -t opendronemap/webodm_slate .
 
 REM Launching server
-docker run -ti --rm -v "%CD%":/webodm/slate -p 4567:4567 opendronemap/webodm_slate --watcher-force-polling
+docker run -ti --rm -v "%CD%":/webodm/slate -p 4567:4567 --entrypoint bash opendronemap/webodm_slate -c "rm Gemfile.lock && bundle install && bundle exec middleman server --watcher-force-polling"
