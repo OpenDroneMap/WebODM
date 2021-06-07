671189b769b7ea1ec1a0b503e6a37107305c530c
Piero Toffanin <pt@masseranolabs.com>
2021-06-07 11:02:26 -0400

    updated slate files

--- slate/lib/multilang.rb
+++ slate/lib/multilang.rb
@@ -1,9 +1,13 @@
 module Multilang
   def block_code(code, full_lang_name)
-    parts = full_lang_name.split('--')
-    rouge_lang_name = parts[0] || ""
-    super(code, rouge_lang_name).sub("highlight #{rouge_lang_name}") do |match|
-      match + " tab-" + full_lang_name
+    if full_lang_name
+      parts = full_lang_name.split('--')
+      rouge_lang_name = (parts) ? parts[0] : "" # just parts[0] here causes null ref exception when no language specified
+      super(code, rouge_lang_name).sub("highlight #{rouge_lang_name}") do |match|
+        match + " tab-" + full_lang_name
+      end
+    else
+      super(code, full_lang_name)
     end
   end
 end
