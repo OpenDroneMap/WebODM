421306a3e8c53fc6aa6fffea9a16d03130362533
Piero Toffanin <pt@masseranolabs.com>
2021-09-26 15:04:03 -0400

    Fix GCPPopup js test for real

--- app/static/app/js/components/tests/GCPPopup.test.jsx
+++ app/static/app/js/components/tests/GCPPopup.test.jsx
@@ -4,7 +4,7 @@ import GCPPopup from '../GCPPopup';
 
 describe('<GCPPopup />', () => {
     it('renders without exploding', () => {
-      const wrapper = mount(<GCPPopup task={{id: 1, project: 1}} feature={{properties: {id: "test", observations: []}}} />);
+      const wrapper = mount(<GCPPopup task={{id: 1, project: 1}} feature={{properties: {id: "test", error: [0,1,2], observations: [{shot_id: "test", annotated: [0, 0], reprojected: [0,0]}]}}} />);
       expect(wrapper.exists()).toBe(true);
     })
   });
