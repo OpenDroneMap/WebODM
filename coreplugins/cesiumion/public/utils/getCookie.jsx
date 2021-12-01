3f939eeae8fcb41874e7e8ee18466558b40eb746
Piero Toffanin <pt@masseranolabs.com>
2021-12-01 09:33:43 -0500

    Archive cesium ion plugin

--- coreplugins/cesiumion/public/utils/getCookie.jsx
+++
@@ -1,9 +0,0 @@
-export default function getCookie(name) {
-	const value = `; ${document.cookie}`;
-	const parts = value.split(`; ${name}=`);
-	if (parts.length == 2)
-		return parts
-			.pop()
-			.split(";")
-			.shift();
-}
