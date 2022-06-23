754c247999e9518b97a369f6f2ae33dcde3949dc
Piero Toffanin <pt@masseranolabs.com>
2022-06-23 11:59:13 -0400

    Re-add modified Multispectral preset

--- app/boot.py
+++ app/boot.py
@@ -99,6 +99,10 @@ def boot():
 
 def add_default_presets():
     try:
+        Preset.objects.update_or_create(name='Multispectral', system=True,
+                                        defaults={'options': [{'name': 'auto-boundary', 'value': True},
+                                                              {'name': 'radiometric-calibration', 'value': 'camera'},
+                                                              ]})
         Preset.objects.update_or_create(name='Volume Analysis', system=True,
                                         defaults={'options': [{'name': 'auto-boundary', 'value': True},
                                                               {'name': 'dsm', 'value': True},
