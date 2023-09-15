788d62c5404c52efafde5c20f5170398ca5d4e3b
Piero Toffanin <pt@masseranolabs.com>
2023-09-15 17:20:29 -0400

    Remove ifs

--- app/models/profile.py
+++ app/models/profile.py
@@ -72,13 +72,9 @@ else:
         
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
-        if settings.FLUSHING: return
-
         if created:
             Profile.objects.create(user=instance)
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
-        if settings.FLUSHING: return
-        
         instance.profile.save()
