c28d00f0b02eb9aea45d4c5a30ffd9c05fd9e783
Piero Toffanin <pt@masseranolabs.com>
2023-11-14 16:10:16 -0500

    10x volume calculations, remove grass dependency

--- coreplugins/measure/api.py
+++ coreplugins/measure/api.py
@@ -1,24 +1,18 @@
 import os
-import json
-import math
 from rest_framework import serializers
 from rest_framework import status
 from rest_framework.response import Response
-import rasterio
-
 from app.api.workers import GetTaskResult, TaskResultOutputError, CheckTask
 from app.models import Task
 from app.plugins.views import TaskView
-
-from worker.tasks import execute_grass_script
-
-from app.plugins.grass_engine import grass, GrassEngineException, cleanup_grass_context
-from geojson import Feature, Point, FeatureCollection
 from django.utils.translation import gettext_lazy as _
+from app.plugins.worker import run_function_async
 
-class GeoJSONSerializer(serializers.Serializer):
-    area = serializers.JSONField(help_text="Polygon contour defining the volume area to compute")
+from .volume import calc_volume
 
+class VolumeRequestSerializer(serializers.Serializer):
+    area = serializers.JSONField(help_text="GeoJSON Polygon contour defining the volume area to compute")
+    method = serializers.CharField(help_text="One of: [plane,triangulate,average,custom,highest,lowest]", default="triangulate", allow_blank=True)
 
 class TaskVolume(TaskView):
     def post(self, request, pk=None):
@@ -26,56 +20,26 @@ class TaskVolume(TaskView):
         if task.dsm_extent is None:
             return Response({'error': _('No surface model available. From the Dashboard, select this task, press Edit, from the options make sure to check "dsm", then press Restart --> From DEM.')})
 
-        serializer = GeoJSONSerializer(data=request.data)
+        serializer = VolumeRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         area = serializer['area'].value
-        points = FeatureCollection([Feature(geometry=Point(coords)) for coords in area['geometry']['coordinates'][0]])
+        method = serializer['method'].value
+        points = [coord for coord in area['geometry']['coordinates'][0]]
         dsm = os.path.abspath(task.get_asset_download_path("dsm.tif"))
 
-        try:
-            context = grass.create_context({'auto_cleanup': False})
-            context.add_file('area_file.geojson', json.dumps(area))
-            context.add_file('points_file.geojson', str(points))
-            context.add_param('dsm_file', dsm)
-            context.set_location(dsm)
-
-            celery_task_id = execute_grass_script.delay(os.path.join(
-                os.path.dirname(os.path.abspath(__file__)),
-                "calc_volume.py"
-            ), context.serialize()).task_id
-
+        try: 
+            celery_task_id = run_function_async(calc_volume, input_dem=dsm, pts=points, pts_epsg=4326, base_method=method).task_id
             return Response({'celery_task_id': celery_task_id}, status=status.HTTP_200_OK)
-        except GrassEngineException as e:
+        except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_200_OK)
 
 class TaskVolumeCheck(CheckTask):
-    def on_error(self, result):
-        cleanup_grass_context(result['context'])
+    pass
 
 class TaskVolumeResult(GetTaskResult):
     def get(self, request, pk=None, celery_task_id=None):
         task = Task.objects.only('dsm_extent').get(pk=pk)
         return super().get(request, celery_task_id, task=task)
 
-    def handle_output(self, output, result, task):
-        cleanup_grass_context(result['context'])
-
-        cols = output.split(':')
-        if len(cols) == 7:
-            # Legacy: we had rasters in EPSG:3857 for a while
-            # This could be removed at some point in the future
-            # Correct scale measurement for web mercator
-            # https://gis.stackexchange.com/questions/93332/calculating-distance-scale-factor-by-latitude-for-mercator#93335
-            scale_factor = 1.0
-            dsm = os.path.abspath(task.get_asset_download_path("dsm.tif"))
-            with rasterio.open(dsm) as dst:
-                if str(dst.crs) == 'EPSG:3857':
-                    latitude = task.dsm_extent.centroid[1]
-                    scale_factor = math.cos(math.radians(latitude)) ** 2
-
-            volume = abs(float(cols[6]) * scale_factor)
-            return str(volume)
-        else:
-            raise TaskResultOutputError(output)
 
