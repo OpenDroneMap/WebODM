bac5050f1ca4abfb3cf3cb65fd6a4b925b0cee11
CodeWracker <dev.rodrigofs@gmail.com>
2024-03-19 10:51:15 -0300

    adicionei no dockerignore algumas pastas que não precsa e coloquei para ignorar toda e qualquer node_modules

--- nodeodm/apps.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-class NodeodmConfig(AppConfig):
-    name = 'nodeodm'
-    verbose_name = _('Node Management')
--- nodeodm/status_codes.py
+++
@@ -1,6 +0,0 @@
-from pyodm.types import TaskStatus
-QUEUED = TaskStatus.QUEUED.value
-RUNNING = TaskStatus.RUNNING.value
-FAILED = TaskStatus.FAILED.value
-COMPLETED = TaskStatus.COMPLETED.value
-CANCELED = TaskStatus.CANCELED.value
