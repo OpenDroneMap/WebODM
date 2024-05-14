351dd88c3d3b0b2b0b1f475139cf35f3f7dd0807
arty-aeroai <arturo@aeroai.io>
2024-05-13 22:29:34 -0700

    cleaned up file

--- coreplugins/cesiumion/public/components/IonAssetButton.jsx
+++ coreplugins/cesiumion/public/components/IonAssetButton.jsx
@@ -1,7 +1,4 @@
 import React, { PureComponent, Fragment } from "react";
-
-import { DropdownButton, MenuItem } from "react-bootstrap";
-
 import IonAssetLabel from "./IonAssetLabel";
 import { AssetStyles } from "../defaults";
 
@@ -15,48 +12,6 @@ export default class IonAssetButton extends PureComponent {
 	};
 
 	handleClick = asset => () => this.props.onSelect(asset);
-
-	// render() {
-	// 	const {
-	// 		assets,
-	// 		onSelect,
-	// 		children,
-	// 		assetComponent: AssetComponent
-	// 	} = this.props;
-
-	// 	const menuItems = assets
-	// 		.sort((a, b) =>
-	// 			AssetStyles[a].name.localeCompare(AssetStyles[b].name)
-	// 		)
-	// 		.map(asset => (
-	// 			<MenuItem
-	// 				key={asset}
-	// 				tag={"a"}
-	// 				onClick={this.handleClick(asset)}
-	// 			>
-	// 				<AssetComponent asset={asset} showIcon={true} />
-	// 			</MenuItem>
-	// 		));
-
-	// 	const title = (
-	// 		<Fragment>
-	// 			<i className={"fa fa-cesium"} />
-	// 			{children}
-	// 		</Fragment>
-	// 	);
-
-	// 	return (
-	// 		<DropdownButton
-	// 			id={"cesiumIonUploadDropdown"}
-	// 			bsStyle={"primary"}
-	// 			bsSize={"small"}
-	// 			className={"ion-btn"}
-	// 			title={title}
-	// 		>
-	// 			{menuItems}
-	// 		</DropdownButton>
-	// 	);
-	// }
 
 	render() {
 		const {
