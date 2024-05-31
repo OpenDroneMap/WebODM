df1a17ddaafd10ae8cf7e4b8b86a508669c8c81e
Fabioomega <fabioms2002@gmail.com>
2024-05-31 12:50:44 -0300

    Fixed endpoint and added better error handling

--- app/api/ai.py
+++ app/api/ai.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import exceptions
 from rest_framework import status
@@ -9,7 +9,9 @@ from rest_framework import exceptions
 from app import models
 import requests
 
-from webodm.settings import EXTERNAL_API_ADDRESS, DISABLE_PERMISSIONS
+from webodm.settings import DISABLE_PERMISSIONS, AGROSMART_API_ADDRESS
+
+import json
 
 def crash_with_style(message: str, status):
     return Response({
@@ -17,10 +19,10 @@ def crash_with_style(message: str, status):
     }, status = status)
 
 def send_post_to_processing(typ: str, subtype: str, project_pk, pk, payload: dict):
-    if not EXTERNAL_API_ADDRESS:
-        return crash_with_style('No <EXTERNAL_API_ADDRESS> was supplied.', status.HTTP_500_INTERNAL_SERVER_ERROR)
+    if not AGROSMART_API_ADDRESS:
+        return crash_with_style('No <AGROSMART_API_ADDRESS> was supplied.', status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    url = f'{EXTERNAL_API_ADDRESS}/process/{typ}'
+    url = f'{AGROSMART_API_ADDRESS}/process/{typ}'
     if subtype:
         url += f'/{subtype}'
     headers = {'content-type': 'application/json'}
@@ -28,12 +30,19 @@ def send_post_to_processing(typ: str, subtype: str, project_pk, pk, payload: dic
         'project_id': project_pk,
         'task_id' : pk
     }
-    response = requests.post(url, data=payload, params=params, headers=headers)
-    return Response(response.json(), status=response.status_code)
+    response = requests.post(url=url, params=params, data=json.dumps(payload), headers=headers)
+    try:
+        res = response.json()
+        return Response(res, status=response.status_code)
+    except ValueError:
+        url_tail = f'{typ}'
+        if subtype:
+            url_tail += f'/{subtype}'
+        return crash_with_style(f"The '/{url_tail}' endpoint does not exist!", status.HTTP_400_BAD_REQUEST)
 
 class AiProcessing(APIView):
 
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
 
     def post(self, request, project_pk, pk):
 
@@ -53,4 +62,4 @@ class AiProcessing(APIView):
             payload = request.data.get('payload', "")
             return send_post_to_processing(typ, sub_type, project_pk, pk, payload)
         else:
-            return crash_with_style(f"You don't have permission to access project: {project_pk}", status.HTTP_401_UNAUTHORIZED)
+            return crash_with_style(f"You don't have permission to access project: {project_pk}", status.HTTP_401_UNAUTHORIZED) 
