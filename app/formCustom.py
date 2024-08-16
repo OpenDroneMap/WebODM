e8f3e12f74a0dc4ae1e5e20af889bff901b13aee
IgorFBastos <igordrbastos@gamil.com>
2024-08-16 10:37:00 -0300

    updated font and details in measure pop up and accounts page

--- app/formCustom.py
+++ app/formCustom.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, label="E-mail")
     full_name = forms.CharField(max_length=150, required=True, label="Nome Completo")
 
     class Meta:
