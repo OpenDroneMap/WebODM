d694ed84a25b6c98b4f823cbc96639af1717b0c4
Ntkskwk <natsukawa247@outlook.com>
2024-08-20 04:47:30 +0800

    refactor: ingore authentication.py for rest_framework_jwt.authentication

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,6 +1,8 @@
-from rest_framework_jwt.authentication import BaseJSONWebTokenAuthentication
-
-
-class JSONWebTokenAuthenticationQS(BaseJSONWebTokenAuthentication):
-    def get_jwt_value(self, request):
-         return request.query_params.get('jwt')
+# from rest_framework_jwt.authentication import BaseJSONWebTokenAuthentication
+#
+# jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
+# jwt_get_username_from_payload_handler = api_settings.JWT_PAYLOAD_GET_USERNAME_HANDLER
+#
+# class JSONWebTokenAuthenticationQS(BaseJSONWebTokenAuthentication):
+#     def get_jwt_value(self, request):
+#          return request.query_params.get('jwt')
