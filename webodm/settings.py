659fed7a674ff1962eba57f4e02faca56f421bd8
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-05-15 11:50:50 +0100

    Various ruff fixups.

--- webodm/settings.py
+++ webodm/settings.py
@@ -418,11 +418,11 @@ if TESTING or FLUSHING:
     EXTERNAL_AUTH_ENDPOINT = 'http://0.0.0.0:5555/auth'
 
 try:
-    from .local_settings import *
+    from .local_settings import *  # noqa: F403
 except ImportError:
     pass
 
 try:
-    from .settings_override import *
+    from .settings_override import *  # noqa: F403
 except ImportError:
     pass
