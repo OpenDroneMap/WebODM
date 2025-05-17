b8486e77fbaa1fc698f02e67d71dfd2b51d94949
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-05-17 16:07:51 +0100

    Remove wildcard import/export.

--- app/plugins/__init__.py
+++ app/plugins/__init__.py
@@ -1,5 +1,11 @@
 from .data_store import GlobalDataStore, UserDataStore
-from .functions import *
+from .functions import (
+    init_plugins, sync_plugin_db, clear_plugins_cache, build_plugins,
+    webpack_watch_process_count, register_plugins, valid_plugin,
+    get_plugins, get_active_plugins, get_plugin_by_name, get_current_plugin,
+    get_plugins_paths, get_plugins_persistent_path, get_dynamic_script_handler,
+    enable_plugin, disable_plugin, delete_plugin, get_site_settings, versionToInt
+)
 from .menu import Menu
 from .mount_point import MountPoint
 from .plugin_base import PluginBase
@@ -9,4 +15,9 @@ __all__ = [
     'PluginBase',
     'Menu',
     'MountPoint',
+    'init_plugins', 'sync_plugin_db', 'clear_plugins_cache', 'build_plugins',
+    'webpack_watch_process_count', 'register_plugins', 'valid_plugin',
+    'get_plugins', 'get_active_plugins', 'get_plugin_by_name', 'get_current_plugin',
+    'get_plugins_paths', 'get_plugins_persistent_path', 'get_dynamic_script_handler',
+    'enable_plugin', 'disable_plugin', 'delete_plugin', 'get_site_settings', 'versionToInt'
 ]
