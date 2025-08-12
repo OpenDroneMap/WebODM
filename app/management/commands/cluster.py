078947468fe25853a7bdf2443ad1e1a6a05606c4
Piero Toffanin <pt@masseranolabs.com>
2025-08-12 14:09:31 -0400

    Export working

--- app/management/commands/cluster.py
+++ app/management/commands/cluster.py
@@ -4,15 +4,46 @@ import math
 import shutil
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
-from app.models import Project
+from app.models import Project, Task, Preset, PluginDatum
 from webodm import settings
 from django.db import connection
 from django.contrib.auth.models import User
-
+from django.core import serializers
+from guardian.shortcuts import get_users_with_perms
 
 def die(msg):
     print(msg)
     exit(1)
+
+def serialize(obj):
+    data = json.loads(serializers.serialize("json", [obj]))[0]
+    f = data['fields']
+
+    model = data['model']
+    if model == "app.project":
+        f['owner'] = "_"
+        data['permissions'] = []
+        perms = get_users_with_perms(obj, attach_perms=True, with_group_users=False)
+        for user in perms:
+            if user.id == obj.owner.id:
+                data['permissions'] = perms[user]
+            else:
+                print(f"Warning! Permissions for [{obj.name}] ({obj.id}) are related to \"{user.username}\" which is not going to be exported.")
+    elif model == "app.task":
+        f['processing_node'] = None
+    elif model == "app.profile":
+        data['pk'] = None
+        f['user'] = '_'
+    elif model == "auth.user":
+        data['pk'] = None
+    elif model == "app.preset":
+        data['pk'] = None
+        f['owner'] = '_'
+    elif model == "app.plugindatum":
+        data['pk'] = None
+        f['user'] = '_'
+    
+    return data
 
 def export_user(user_id, dry_run=False, cluster_export_dir=None):
     try:
@@ -55,19 +86,11 @@ def export_user(user_id, dry_run=False, cluster_export_dir=None):
 
     if cluster_export_dir is None:
         cluster_export_dir = os.path.join(settings.MEDIA_ROOT, "cluster_migrations")
-    user_export_dir = os.path.join(cluster_export_dir, str(user.id))
+    user_export_dir = os.path.join(cluster_export_dir, str(user.username))
     projects_export_dir = os.path.join(user_export_dir, "projects")
 
     print("Cluster export directory: %s" % cluster_export_dir)
     
-    # Few basic checks
-    if os.path.isdir(cluster_export_dir):
-        contents = list_safe(cluster_export_dir)
-
-        # There should be no folders, or just folders with numbers
-        if len(contents) > 0 and not all([d.isnumeric() for d in contents]):
-            die("Cluster export directory exists and has existing directories that don't match an export location?")
-
     make_dir(cluster_export_dir)
     make_dir(user_export_dir)
     make_dir(projects_export_dir)
@@ -76,20 +99,46 @@ def export_user(user_id, dry_run=False, cluster_export_dir=None):
     print("Projects export directory: %s" % projects_export_dir)
 
     # Get list of projects for this user
-    user_projects = Project.objects.filter(owner=user)
+    user_projects = Project.objects.filter(owner=user).order_by('created_at')
+    user_tasks = Task.objects.filter(project__owner=user).order_by('created_at')
+    user_presets = Preset.objects.filter(owner=user, system=False).order_by('created_at')
+    user_plugindatum = PluginDatum.objects.filter(user=user).order_by('id')
+
     print("Total projects: %s" % len(user_projects))
     print([p.id for p in user_projects])
+
+    print("Total tasks: %s" % len(user_tasks))
+    print("Total presets: %s" % len(user_presets))
+    print("Total plugin data: %s" % len(user_plugindatum))
+    
 
     if len(list_safe(projects_export_dir)) > 0:
         print("Export directory not empty, removing/recreating")
         remove_dir(projects_export_dir)
         make_dir(projects_export_dir)
     
+    db = {
+        'projects': [serialize(p) for p in user_projects],
+        'tasks': [serialize(t) for t in user_tasks],
+        'profile': serialize(user.profile),
+        'user': serialize(user),
+        'presets': [serialize(p) for p in user_presets],
+        'plugin_datum': [serialize(pd) for pd in user_plugindatum]
+    }
+
+    db_dump_file = os.path.join(user_export_dir, "db.json")
+    db_dump = json.dumps(db)
+    
+    print("Writing %s" % db_dump_file)
+    if not dry_run:
+        with open(db_dump_file, "w", encoding="utf-8") as f:
+            f.write(db_dump)
+    
     # Copy all project folders (note some do not exist)
     for p in user_projects:
         copy_dir(p.get_project_dir(), os.path.join(projects_export_dir, str(p.id)))
 
-    
+
 
 class Command(BaseCommand):
     requires_system_checks = []
