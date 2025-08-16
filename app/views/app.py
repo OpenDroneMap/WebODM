7833e917e61eb6c7e870d1d8b1826bf67f4db898
Piero Toffanin <pt@masseranolabs.com>
2025-08-16 18:10:55 -0400

    302 cluster redirect for private views

--- app/views/app.py
+++ app/views/app.py
@@ -13,7 +13,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext as _
 from django import forms
-from app.views.utils import get_permissions
+from app.views.utils import get_permissions, get_project_or_raise, get_task_or_raise, handle_302
 from webodm import settings
 
 def index(request):
@@ -60,16 +60,17 @@ def dashboard(request):
 
 
 @login_required
+@handle_302
 def map(request, project_pk=None, task_pk=None):
     title = _("Map")
 
     if project_pk is not None:
-        project = get_object_or_404(Project, pk=project_pk)
+        project = get_project_or_raise(pk=project_pk)
         if not request.user.has_perm('app.view_project', project):
             raise Http404()
         
         if task_pk is not None:
-            task = get_object_or_404(Task.objects.defer('orthophoto_extent', 'dsm_extent', 'dtm_extent'), pk=task_pk, project=project)
+            task = get_task_or_raise(pk=task_pk, project=project)
             title = task.name or task.id
             mapItems = [task.get_map_items()]
             projectInfo = None
@@ -92,16 +93,17 @@ def map(request, project_pk=None, task_pk=None):
 
 
 @login_required
+@handle_302
 def model_display(request, project_pk=None, task_pk=None):
     title = _("3D Model Display")
 
     if project_pk is not None:
-        project = get_object_or_404(Project, pk=project_pk)
+        project = get_project_or_raise(pk=project_pk)
         if not request.user.has_perm('app.view_project', project):
             raise Http404()
 
         if task_pk is not None:
-            task = get_object_or_404(Task.objects.defer('orthophoto_extent', 'dsm_extent', 'dtm_extent'), pk=task_pk, project=project)
+            task = get_task_or_raise(pk=task_pk, project=project)
             title = task.name or task.id
         else:
             raise Http404()
--- app/views/utils.py
+++ app/views/utils.py
@@ -1,6 +1,72 @@
+from django.shortcuts import get_object_or_404
+from django.http import Http404, HttpResponseRedirect
+from app.models import Project, Task, Redirect
+from webodm import settings
+from functools import wraps
+
 def get_permissions(user, project):
     perms = []
     for p in ["view", "change"]:
         if user.has_perm('app.%s_project' % p, project):
             perms.append(p)
     return perms
+
+
+class HttpRedirect302(Exception):
+    def __init__(self, to_cluster):
+        self.to_cluster = to_cluster
+
+
+def handle_302(func):
+    @wraps(func)
+    def wrap(request, *args, **kwargs):
+        try:
+            return func(request, *args, **kwargs)
+        except HttpRedirect302 as r:
+            return HttpResponseRedirect((settings.CLUSTER_URL % r.to_cluster) + request.get_full_path())
+    return wrap
+
+def get_project_or_raise(pk=None, public_id=None):
+    kwargs = {}
+    if pk is not None:
+        kwargs['pk'] = pk
+    if public_id is not None:
+        kwargs['public_id'] = public_id
+    
+    try:
+        return get_object_or_404(Project, **kwargs)
+    except Http404 as err404:
+        # Check for redirects if cluster mode is enabled
+        if settings.CLUSTER_ID is not None:
+            p = {}
+            if pk is not None:
+                p['project_id'] = pk
+            if public_id is not None:
+                p['project_public_id'] = public_id
+
+            try:
+                r = Redirect.objects.get(**p)
+                raise HttpRedirect302(r.cluster_id)
+            except Redirect.DoesNotExist:
+                raise err404
+        else:
+            raise err404
+
+def get_task_or_raise(pk=None, project=None):
+    kwargs = {}
+    if pk is not None:
+        kwargs['pk'] = pk
+    if project is not None:
+        kwargs['project'] = project
+    try:
+        return get_object_or_404(Task, **kwargs)
+    except Http404 as err404:
+        # Check for redirects if cluster mode is enabled
+        if settings.CLUSTER_ID is not None:
+            try:
+                r = Redirect.objects.get(task_id=pk)
+                raise HttpRedirect302(r.cluster_id)
+            except Redirect.DoesNotExist:
+                raise err404
+        else:
+            raise err404
