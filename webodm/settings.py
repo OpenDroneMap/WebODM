011ac34e026dba3cc925cdd909e71d1bddcef4dd
Piero Toffanin <pt@masseranolabs.com>
2026-04-14 22:19:49 -0400

    Use Etc/UTC

--- webodm/settings.py
+++ webodm/settings.py
@@ -165,7 +165,7 @@ AUTHENTICATION_BACKENDS = (
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Etc/UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
